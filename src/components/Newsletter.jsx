7cacab764200bb8d8124caf1fc6df66364e08854
nabil183 <Shariea Reza Nabil>
2025-12-08 00:02:57 +0600

    update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,23 +2,50 @@ import React from 'react'
 
 const Newsletter = () => {
   return (
-    <div className='w-full p-16 text-center text-white flex gap-8 md:flex-row sm:flex-row flex-col items-center justify-around'>
 
-      <div className='text-white'>
-        <p className='font-bold md:text-3xl sm:text-2xl text-[20px]'>Want tips & tricks to optimize your flow?</p>
-        <p className='md:text-[16px] sm:text-[14px] text-[12px]'>Sign up to our newsletter and stay up to date</p>
-      </div>
+    <div className="w-full p-16 text-white">
+  <div className="w-full mx-auto flex flex-col lg:flex-row gap-10 justify-center  text-center lg:text-left">
 
-      <div className='flex flex-col gap-3'>
-        <input type='email' placeholder='Enter your email'className='bg-white text-black outline-none p-3 rounded-[10px]'/>
-        <button className='bg-[#00df9a] p-2 rounded-[10px]'>Notify me</button>
-        <p className='md:text-[16px] sm:text-[14px] text-[12px]'>We care about of your protection of your data</p>
-        <p className='md:text-[16px] sm:text-[14px] text-[12px]'>Read our 
-          <a href='#' className='underline text-[#00df9a] mb-2 '>Privacy Policy</a>
-        </p>
-      </div>
-
+    {/* LEFT */}
+    <div className="w-full lg:w-1/2">
+      <p className="font-bold text-[20px] sm:text-2xl md:text-3xl">
+        Want tips & tricks to optimize your flow?
+      </p>
+      <p className="text-sm sm:text-base md:text-lg mt-2">
+        Sign up to our newsletter and stay up to date
+      </p>
     </div>
+
+    {/* RIGHT */}
+    <div className="flex flex-col ">
+
+      {/* EMAIL INPUT + BUTTON */}
+      <div className="flex flex-col sm:flex-row  items-center lg:items-start gap-3 w-full mb-2">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="bg-white text-black outline-none p-3 rounded-lg w-full sm:w-auto sm:flex-1"
+        />
+        <button className="bg-[#00df9a] md:p-3 sm:p-3 py-2 rounded-lg w-full sm:w-auto font-semibold">
+          Notify me
+        </button>
+      </div>
+
+      <p className="text-xs sm:text-sm md:text-base">
+        We care about the protection of your data.
+      </p>
+      <p className="text-xs sm:text-sm md:text-base text-center lg:text-left">
+        Read our
+        <a href="#" className="underline text-[#00df9a] ml-1">
+          Privacy Policy
+        </a>
+      </p>
+    </div>
+
+  </div>
+</div>
+
+
   )
 }
 
